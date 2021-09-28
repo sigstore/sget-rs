7c3bc63c93d272c4777cfd2a862d5bb5d3f4a03d
jyotsna-penumaka <jpenumak@redhat.com>
2021-09-28 09:34:14 -0400

    Add a more informative error message
    Signed-off-by: jyotsna-penumaka <jpenumak@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     // Example : ./sget --noExec --outFile /home/jyotsna/scripts https://cdn.jsdelivr.net/npm/vue/dist/vue.js 
 
     let args: Vec<String> = env::args().collect();
-    let url = Url::parse(&args[(args.len()) - 1]).expect("Please provide a valid URL");
+    let url = Url::parse(&args[(args.len()) - 1]).expect("Please provide a valid URL with the correct scheme (http/https/www etc.)");
     println!("URL is: {}", url);
 
     let mut lowercase_element;
